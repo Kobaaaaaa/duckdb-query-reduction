610ee619764ae7a4fc035a18a0e8745ea7bbd9d7
Ilias Bouraoui <ilias.bouraoui@epfl.ch>
2026-02-11 23:50:57 -0500

    Added queries 6 and 7

--- flock-llm-reduction/sql/10_benchmarks_sql/q06_coauthor_pairs.sql
+++ flock-llm-reduction/sql/10_benchmarks_sql/q06_coauthor_pairs.sql
@@ -1,9 +1,8 @@
 -- Top 20 co-author pairs (by number of shared works)
 SELECT a1.author_name AS author_1, a2.author_name AS author_2, COUNT(*) AS n_shared_works
 FROM work_authors wa1
-JOIN work_authors wa2
-  ON wa1.doi = wa2.doi
- AND wa1.author_id < wa2.author_id
+JOIN work_authors wa2 
+  ON wa1.doi = wa2.doi AND wa1.author_id < wa2.author_id
 JOIN authors a1 ON a1.author_id = wa1.author_id
 JOIN authors a2 ON a2.author_id = wa2.author_id
 GROUP BY a1.author_name, a2.author_name
--- flock-llm-reduction/sql/10_benchmarks_sql/q07_special_citations.sql
+++ flock-llm-reduction/sql/10_benchmarks_sql/q07_special_citations.sql
@@ -1,14 +1,5 @@
--- Checks if works have multiple authors
-SELECT doi, COUNT(*) AS n_authors
-FROM work_authors
-GROUP BY doi
-HAVING COUNT(*) > 1
-ORDER BY n_authors DESC, doi;
-
--- Checks if there are citations where the citing or cited DOI is missing from the works table
-SELECT
-  SUM(CASE WHEN w1.doi IS NULL THEN 1 ELSE 0 END) AS missing_citing,
-  SUM(CASE WHEN w2.doi IS NULL THEN 1 ELSE 0 END) AS missing_cited
-FROM citations c
-LEFT JOIN works w1 ON w1.doi = c.citing_doi
-LEFT JOIN works w2 ON w2.doi = c.cited_doi;
+-- Works for which an author or journal are both cited and citing, (i.e., self-citations)
+SELECT citing_doi, citing_title, cited_doi,  cited_title, journal_sc, author_sc
+FROM citations_enriched
+WHERE journal_sc_bool OR author_sc_bool
+ORDER BY citing_doi, cited_doi;
