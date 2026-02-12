534527063c8f00d9ab4ab498a635dcc46963d1ef
Ilias Bouraoui <ilias.bouraoui@epfl.ch>
2026-02-12 00:03:09 -0500

    Added first flock query

---
+++ flock-llm-reduction/sql/20_benchmarks_llm/q08_llm_topic_tag.sql
@@ -0,0 +1,16 @@
+-- Generate topic labels for all works using 'llm_complete'
+SELECT
+  doi,
+  title,
+  venue,
+  llm_complete(
+    {'model_name':'my_model'},
+    {
+      'prompt': 'Return a 3-6 word topic label for this paper.',
+      'context_columns': [
+        {'data': title, 'name':'title'},
+        {'data': venue, 'name':'venue'}
+      ]
+    }
+  ) AS topic_label
+FROM works;
