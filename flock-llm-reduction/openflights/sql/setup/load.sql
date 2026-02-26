8d6bb29178d4ce6f3009eea6045c792cba3a17c2
Ilias Bouraoui <ilias.bouraoui@epfl.ch>
2026-02-25 22:53:16 -0500

    Added samples for each of the openflights tables for visualization

--- flock-llm-reduction/openflights/sql/setup/load.sql
+++ flock-llm-reduction/openflights/sql/setup/load.sql
@@ -3,21 +3,17 @@ LOAD flock;
 
 .read '.\local\secrets.sql'
 
--- Choose dataset folder (edit this ONE LINE)
--- Use: 'data/original_data'  OR  'data/samples'
-CREATE OR REPLACE MACRO data_dir() AS 'data/original_data';
-
 CREATE OR REPLACE TABLE airports AS
-SELECT * FROM read_csv_auto(data_dir() || '/airports.csv', all_varchar=true);
+SELECT * FROM read_csv_auto('data/original_data/airports.csv', all_varchar=true);
 
 CREATE OR REPLACE TABLE airlines AS
-SELECT * FROM read_csv_auto(data_dir() || '/airlines.csv', all_varchar=true);
+SELECT * FROM read_csv_auto('data/original_data/airlines.csv', all_varchar=true);
 
 CREATE OR REPLACE TABLE routes AS
-SELECT * FROM read_csv_auto(data_dir() || '/routes.csv', all_varchar=true);
+SELECT * FROM read_csv_auto('data/original_data/routes.csv', all_varchar=true);
 
 CREATE OR REPLACE TABLE planes AS
-SELECT * FROM read_csv_auto(data_dir() || '/planes.csv', all_varchar=true);
+SELECT * FROM read_csv_auto('data/original_data/planes.csv', all_varchar=true);
 
 CREATE OR REPLACE TABLE countries AS
-SELECT * FROM read_csv_auto(data_dir() || '/countries.csv', all_varchar=true);
+SELECT * FROM read_csv_auto('data/original_data/countries.csv', all_varchar=true);
